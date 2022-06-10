7edea75fb9104576d5ee3bc2174220274f6d5191
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-06-09 23:23:53 -0700

    z8z8z8z

--- Account/urls.py
+++ Account/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path,include
+from django.urls import path, include
 from .import views
 urlpatterns = [
-    path('',views.login_view,name="login"),
-    path('Create_SuperUser',views.SuperUser_CreateView,name="register"),
-    
+    path('', views.login_view, name="login"),
+    path('log_out', views.logout_view, name="logout"),
+    path('Create_SuperUser', views.SuperUser_CreateView, name="register"),
+
 ]
