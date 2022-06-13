90e8772d21a1874dd656a9868802dcd618c90408
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-06-12 17:52:29 -0700

    forgot password added by ipha

--- Account/urls.py
+++ Account/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('Create_SuperUser/', views.SuperUser_CreateView, name="register"),
 
     #password reseting area
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
-    path('reset_password/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='Account/password_reset.html'),name='reset_password'),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='Account/password_reset_sent.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='Account/password_reset_form.html'),name='password_reset_confirm'),
+    #,success_url=reverse_lazy('password_reset_complete')
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='Account/password_reset_done.html'),name='password_reset_complete'),
 
 ]
--- BGI/settings.py
+++ BGI/settings.py
@@ -132,5 +132,5 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='ephadaniel177@gmail.com'
-EMAIL_HOST_PASSWORD='629177490ipHA++'
+EMAIL_HOST_PASSWORD='yheffqwwlrjwpqkg'
 
